babebae07902c675bd3a23db6c54eac11905437e
Pastitas <rootwow@gmail.com>
2020-05-31 16:13:55 +0200

    offsets for new inductive mount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -962,7 +962,8 @@
  * Specify a Probe position as { X, Y, Z }
  */
 //#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
-#define NOZZLE_TO_PROBE_OFFSET { -42, -16, 0 }
+//inductivo new
+#define NOZZLE_TO_PROBE_OFFSET { 27, -8, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
