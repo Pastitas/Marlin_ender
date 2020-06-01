b6faa4a83a58b720bb141109f097a23fd680c812
Pastitas <rootwow@gmail.com>
2020-06-01 12:13:42 +0200

    deactivated bed leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1213,7 +1213,7 @@
 //#define AUTO_BED_LEVELING_LINEAR
 // ACTIVAR_ABL #define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
-#define MESH_BED_LEVELING
+// #define MESH_BED_LEVELING
 
 /**
  * Normally G28 leaves leveling disabled on completion. Enable
@@ -1318,7 +1318,7 @@
  * Add a bed leveling sub-menu for ABL or MBL.
  * Include a guided procedure if manual probing is enabled.
  */
-#define LCD_BED_LEVELING
+// ACTIVAR_ABL #define LCD_BED_LEVELING
 
 #if ENABLED(LCD_BED_LEVELING)
   #define MESH_EDIT_Z_STEP  0.025 // (mm) Step size while manually probing Z axis.
